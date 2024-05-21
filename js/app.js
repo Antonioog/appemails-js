c839484e9a17b76b4c804860895b6ca543c39a72
Antonioog <arkanamos1985@gmail.com>
2024-05-21 13:43:40 -0600

    se agregar las alertas de validacion en inputs

--- js/app.js
+++ js/app.js
@@ -3,17 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
   //Agregamos los addevenlisteners...
-  inputEmail.addEventListener("blur", function () {
-    console.log("Sali del input Email");
-  });
+  inputEmail.addEventListener("blur", validar);
+  inputAsunto.addEventListener("blur", validar);
+  inputMensaje.addEventListener("blur", validar);
 
-  inputAsunto.addEventListener("blur", function () {
-    console.log("Sali del input Asunto");
-  });
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      mostrarAlerta();
+    } else {
+      console.log("El campo tiene contenido");
+    }
 
-  inputMensaje.addEventListener("blur", function () {
-    console.log("Sali del input Mensaje");
-  });
+    function mostrarAlerta() {
+      //Generar alerta en HTML...
+      const error = document.createElement("P");
+      error.textContent = "Hubo un error";
+      //Agregamos estilos al parrafo que creamos de la alerta...
+      error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+      //Inyectar el error al formulario...
+      formulario.appendChild(error);
+    }
+  }
 });
