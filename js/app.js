b27322f5044d3817fa1fb2c78b197d580ccfc162
Antonioog <arkanamos1985@gmail.com>
2024-05-28 22:33:57 -0600

    Se Eliminan las alertas duplicadas

--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //Sleccionamos los elementos de la interfaz...
+  //Seleccionamos los elementos de la interfaz...
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
   const formulario = document.querySelector("#formulario");
 
-  //Agregamos los addevenlisteners...
+  //Asignamos los EventListeners...
   inputEmail.addEventListener("blur", validar);
   inputAsunto.addEventListener("blur", validar);
   inputMensaje.addEventListener("blur", validar);
 
+  //Creamos una funcion de validar para simplificar codigo de validacion...
   function validar(e) {
     if (e.target.value.trim() === "") {
-      mostrarAlerta();
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
     } else {
-      console.log("El campo tiene contenido");
+      console.log("Campo con informacion");
     }
-
-    function mostrarAlerta() {
-      //Generar alerta en HTML...
-      const error = document.createElement("P");
-      error.textContent = "Hubo un error";
-      //Agregamos estilos al parrafo que creamos de la alerta...
-      error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
-
-      //Inyectar el error al formulario...
-      formulario.appendChild(error);
+  }
+  //Creamos uns funcion para que muestre la alerta en cada error...
+  function mostrarAlerta(mensaje, referencia) {
+    //Comprueba si ya existe una alerta..
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
     }
+    //Creamos el HTML de la alerta...
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+    //Inyectamos el mensaje en el formulario...
+    referencia.appendChild(error);
   }
 });
